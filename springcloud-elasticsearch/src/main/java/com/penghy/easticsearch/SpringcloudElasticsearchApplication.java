9655b3b28c93eff515db24bcc293c07a2f9b34c5
penghy <13735485749@163.com>
2021-04-01 13:34:21 +0800

    改动

--- springcloud-elasticsearch/src/main/java/com/penghy/easticsearch/SpringcloudElasticsearchApplication.java
+++ springcloud-elasticsearch/src/main/java/com/penghy/easticsearch/SpringcloudElasticsearchApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringcloudElasticsearchApplication {
 
+    // gdiogn
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudElasticsearchApplication.class, args);
     }
