54c74481706f7da5f2e711107cc2b90cfc6bef90
penghy <13735485749@163.com>
2021-04-16 17:09:35 +0800

    new springcloud-test-server

---
+++ springcloud-test-server/src/main/java/com/penghy/server/SpringcloudTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.penghy.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudTestServerApplication.class, args);
+    }
+
+}
